f95c1a71f3908b2df9b7a48b48ab289f8321aaa5
Админ <matroskin8@i.ua>
2023-09-18 19:30:42 +0300

    Check php version

--- incs/config.php
+++ incs/config.php
@@ -1,4 +1,8 @@
 <?php
 
+if (version_compare(phpversion(), '8.2.0', '<') == true) {
+    exit('Check PHP version, please. PHP 8.2+ Required');
+}
+
 define("ROOT", dirname(__DIR__));
 const VIEWS = ROOT . '/views';
